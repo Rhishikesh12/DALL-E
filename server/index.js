c71245d73ba5ca02661a2f4591620e0a197e5cf7
Rhishikesh12 <rhishikeshchaughule@gmail.com>
2024-12-30 16:15:36 +0530

    gr

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import connectDB from "./mongodb/connect.js";
 import postRoutes from "./routes/postRoutes.js";
 import dalleRoutes from "./routes/dalleRoutes.js";
 
-Hello world
+Hello Thakur
 
 
 dotenv.config();
